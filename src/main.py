20ebf41975d2f7f583dcc3c1c9e78190b77c16bb
msusanrbrown <sbrow120@fau.edu>
2021-08-24 16:17:48 +0000

    first commit

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Vehicle
 #from models import Person
 
 app = Flask(__name__)
@@ -32,12 +32,101 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
-
     return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def people():
+    all_people=People.query.all()
+    response_body = [x.serialize() for x in all_people]
+    return jsonify(response_body), 200
+
+@app.route('/people/<int:id>', methods=['GET'])
+def people_by_id(id):
+    people=People.query.filter(People.id==id).first_or_404(description=f'Record with id={id} is not available'.format(id))
+    response_body = people.serialize()
+    return jsonify(response_body), 200
+
+@app.route('/people', methods=['POST'])
+def people_edit():
+    args = request.get_json(silent=True)
+    people = People(name=args['name'], birth_year=args['birth_year'], eye_color=args['eye_color'],
+            homeworld_id=args['homeworld_id'], gender=args['gender'], 
+            hair_color=args['hair_color'], height=args['height'], mass=args['mass'], 
+            skin_color=args['skin_color'], created=args['created'], edited=args['edited'])
+    db.session.add(people)
+    db.session.commit()
+    return People.serialize(people), 201
+
+@app.route('/people/<int:id>', methods=['DELETE'])
+def delete_people_by_id(id):
+    record = People.query.filter(People.id==id).first_or_404(description=f'Record with id={id} is not available'.format(id))
+    db.session.delete(record)
+    db.session.commit()
+    return '', 204
+
+
+@app.route('/planet', methods=['GET'])
+def planet():
+    all_planet=Planet.query.all()
+    response_body = [x.serialize() for x in all_planet]
+    return jsonify(response_body), 200
+
+@app.route('/planet/<int:id>', methods=['GET'])
+def planet_by_id(id):
+    planet=Planet.query.filter(Planet.id==id).first_or_404(description=f'Record with id={id} is not available'.format(id))
+    response_body = planet.serialize()
+    return jsonify(response_body), 200
+
+@app.route('/planet', methods=['POST'])
+def planet_edit():
+    args = request.get_json(silent=True)
+    planet = Planet(name=args['name'], diameter=args['diameter'], rotation_period=args['rotation_period'],
+            orbital_period=args['orbital_period'], gravity=args['gravity'], population=args['population'], 
+            climate=args['climate'], terrain=args['terrain'], surface_water=args['surface_water'], 
+            created=args['created'], edited=args['edited'])
+    db.session.add(planet)
+    db.session.commit()
+    return Planet.serialize(planet), 201
+
+@app.route('/planet/<int:id>', methods=['DELETE'])
+def delete_planet_by_id(id):
+    record = Planet.query.filter(Planet.id==id).first_or_404(description=f'Record with id={id} is not available'.format(id))
+    db.session.delete(record)
+    db.session.commit()
+    return '', 204
+
+@app.route('/vehicle', methods=['GET'])
+def vehicle():
+    all_vehicle=Planet.query.all()
+    response_body = [x.serialize() for x in all_vehicle]
+    return jsonify(response_body), 200
+
+@app.route('/vehicle/<int:id>', methods=['GET'])
+def vehicle_by_id(id):
+    vehicle=Vehicle.query.filter(Vehicle.id==id).first_or_404(description=f'Record with id={id} is not available'.format(id))
+    response_body = vehicle.serialize()
+    return jsonify(response_body), 200
+
+@app.route('/vehicle', methods=['POST'])
+def vehicle_edit():
+    args = request.get_json(silent=True)
+    vehicle = Vehicle(name=args['name'], model=args['model'], vehicle_class=args['vehicle_class'],
+            manufacturer=args['manufacturer'], length=args['length'], cost_in_credits=args['cost_in_credits'], 
+            crew=args['crew'], passengers=args['passengers'], max_atmosphering_speed=args['max_atmosphering_speed'], 
+            cargo_capacity=args['cargo_capacity'], consumables=args['consumables'], created=args['created'], edited=args['edited'])
+    db.session.add(vehicle)
+    db.session.commit()
+    return Vehicle.serialize(vehicle), 201
+
+@app.route('/vehicle/<int:id>', methods=['DELETE'])
+def delete_vehicle_by_id(id):
+    record = Vehicle.query.filter(Vehicle.id==id).first_or_404(description=f'Record with id={id} is not available'.format(id))
+    db.session.delete(record)
+    db.session.commit()
+    return '', 204
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -8,12 +8,110 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String(250))
+    diameter = db.Column(db.String(250))
+    rotation_period = db.Column(db.String(250))
+    orbital_period = db.Column(db.String(250))
+    gravity = db.Column(db.String(250))
+    population = db.Column(db.String(250))
+    climate = db.Column(db.String(250))
+    terrain = db.Column(db.String(250))
+    surface_water = db.Column(db.String(250))
+    created = db.Column(db.String(250))
+    edited = db.Column(db.String(250))
+
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<Planet {self.name}-{self.id}> '
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "created": self.created,
+            "edited": self.edited
+            # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    birth_year = db.Column(db.String(250))
+    eye_color = db.Column(db.String(250))
+    homeworld_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    homeworld = db.relationship(Planet)
+    gender = db.Column(db.String(250))
+    hair_color = db.Column(db.String(250))
+    height = db.Column(db.String(250))
+    mass = db.Column(db.String(250))
+    skin_color = db.Column(db.String(250))
+    created = db.Column(db.String(250)) 
+    edited = db.Column(db.String(250))
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "eye_color": self.eye_color,
+            "homeworld_id":self.homeworld_id,
+            "gender":self.gender,
+            "hair_color":self.hair_color,
+            "height":self.height,
+            "mass":self.mass,
+            "skin_color":self.skin_color,
+            "created":self.created,
+            "edited":self.edited
+            # do not serialize the password, its a security breach
+        }
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    model = db.Column(db.String(250))
+    vehicle_class = db.Column(db.String(250))
+    manufacturer = db.Column(db.String(250))
+    length = db.Column(db.String(250))
+    cost_in_credits = db.Column(db.String(250))
+    crew = db.Column(db.String(250))
+    passengers = db.Column(db.String(250))
+    max_atmosphering_speed = db.Column(db.String(250))
+    cargo_capacity = db.Column(db.String(250))
+    consumables = db.Column(db.String(250))
+    created = db.Column(db.String(250))
+    edited = db.Column(db.String(250))
+
+
+    def __repr__(self):
+        return '<Vehicle %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "vehicle_class":self.vehicle_class,
+            "manufacturer":self.manufacturer,
+            "length":self.length,
+            "cost_in_credits":self.cost_in_credits,
+            "crew":self.crew,
+            "passengers":self.passengers,
+            "max_atmosphering_speed":self.max_atmosphering_speed,
+            "cargo_capacity":self.cargo_capacity,
+            "consumables":self.consumables,
+            "created":self.created,
+            "edited":self.edited
             # do not serialize the password, its a security breach
         }
